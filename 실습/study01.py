e93723ba239b4b9615e60e2e1a59d4922c6be15a
jisangchung <61720303+jisangchung@users.noreply.github.com>
2020-03-03 12:40:11 +0900

    Create study01.py

---
+++ 실습/study01.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(1+8)
+
+
+# In[3]:
+
+
+print('python')
+
+
+# In[4]:
+
+
+print("python",3)
+
+
+# 메모M
+
+# In[5]:
+
+
+print('i',end=' ')
+print('love')
+print('pyton')
+
+
+# In[6]:
+
+
+x=3
+
+
+# In[8]:
+
+
+print(x)
+
+
+# In[9]:
+
+
+x=8
+y=6
+print(x+y)
+
+
+# In[10]:
+
+
+x=7
+u=5
+print(x/u)
+
+
+# In[12]:
+
+
+x=4
+y=2
+print(x*y)
+
+
+# In[13]:
+
+
+x=7
+y=6
+print(x**y)
+
+
+# In[16]:
+
+
+x=int(input('x의 값을 입력'))
+y=int(input('y의 값을 입력'))
+print(x+y)
+
+
+# In[17]:
+
+
+if False:
+    print(1)
+
+elif True:
+    print(5)
+else:
+    print(3)
+
+
+# In[21]:
+
+
+x=int(input('x'))
+
+if x==5:
+    print('x는 5입니다')
+    
+elif x>5:
+    print('x는 5보다 큽니다')
+    
+else:
+    print('x는 5보다 작습니다')
+    
+
+
+# In[23]:
+
+
+x=int(input('x '))
+y=int(input('y '))
+
+if x>5 and y<3:
+    print(x+y)
+
+elif x>5  and y>=3:
+    print(x-y)
+    
+else:
+    print(x)
+    
+
